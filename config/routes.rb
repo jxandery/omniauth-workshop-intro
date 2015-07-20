10e6aa6666de388cefb4582168d4254da4a4767c
Jack Yeh <jack.yeh@gmail.com>
2015-07-19 21:46:58 -0600

    add logout functionality: - create destroy action - update welcome#index view - create logout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
    root 'welcome#index'
    get '/auth/twitter', as: :login
    get '/auth/twitter/callback', to: 'sessions#create'
+   get 'logout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
