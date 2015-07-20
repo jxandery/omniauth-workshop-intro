418670237c687eb194e87b4be5b68725446cc55b
Jack Yeh <jack.yeh@gmail.com>
2015-07-19 22:00:47 -0600

    setup test: - add capybara gem to the gemfile - create test: loggin in

---
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  def setup
+    Capybara.app = OauthWorkshop::Application
+  end
+
+  test "logging in" do
+    visit "/"
+    assert_equal 200, page.status_code
+
+    click_link "login"
+    assert_equal "/", current_path
+    assert page.has_content?("Jack")
+    assert page.has_link?("logout")
+  end
+end
